fbc854db0a810389fec7d9562bf1a27bc2c330f2
Manish Vyas <manishvyas1911@gmail.com>
2021-11-20 12:34:00 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,132 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+var json2xlsx = require('node-json-xlsx');
+var fs = require('fs');
+var json2xls = require('json2xls');
+
+
+const processData = function (tableData: any, name: String) {
+    let stockPriceCAGRObject = {
+        "StockName": name,
+        "1Year": '0%',
+        "3Years": '0%',
+        "5Years": '0%',
+        "10Years": '0%'
+    };
+    if (tableData[2]) {
+        let stockPriceCAGRStr = tableData[2].trim().replace(/\n+/g, '').replace(/\s+/g, ' ');
+        let stockPriceCAGR = stockPriceCAGRStr.split(/\r?\s/);
+
+        stockPriceCAGR.forEach((cagr: string, i: Number) => {
+            if (i == 5) {
+                stockPriceCAGRObject["10Years"] = cagr;
+            }
+            if (i == 8) {
+                stockPriceCAGRObject["5Years"] = cagr
+            }
+            if (i == 11) {
+                stockPriceCAGRObject["3Years"] = cagr
+            }
+            if (i == 14) {
+                stockPriceCAGRObject["1Year"] = cagr
+            }
+        });
+    }
+
+    return stockPriceCAGRObject;
+}
+
+const crawl = async ({ url, name }: any) => {
+
+    const response = await fetch("https://www.screener.in/company/" + url)
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    console.log($('.ranges-table').text())
+    const tableData = $('.ranges-table')
+        .map((i, table) => {
+            return $(table).text()
+        })
+        .get();
+    console.log(tableData);
+
+    // let compountSalesGrowth = tableData[0].trim().split(/\r?\n/);
+    // let compountProfitGrowth = tableData[1].trim().split(/\r?\n/);
+    //  let ReturnOnEquity = tableData[3].trim().split(/\r?\n/);
+    if (tableData) {
+        return await processData(tableData, name)
+    } else {
+        console.log("Not Processed", name, tableData);
+
+    }
+
+}
+
+async function processStocks(input: any) {
+    let response = [];
+    for (const i of input) {
+        response.push(await crawl(i))
+    }
+
+    return response;
+};
+
+// Main
+var bankInputJSON = require('./input/bankdata.json');
+var fmcgInputJSON = require('./input/fmcgdata.json');
+var itInputJSON = require('./input/itdata.json');
+var pharmaInputJSON = require('./input/pharmadata.json');
+var chemicalInputJSON = require('./input/chemical.json');
+var miscInputJSON = require('./input/misc.json');
+
+const delay = function (ms: number) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+
+processStocks(miscInputJSON).then(value => {
+    let finalResponse = [...value];
+    delay(10000);
+    processStocks(chemicalInputJSON).then(value => {
+        finalResponse = [...value, ...finalResponse]
+        delay(10000);
+        processStocks(pharmaInputJSON).then(value => {
+            finalResponse = [...value, ...finalResponse]
+            delay(10000);
+            processStocks(itInputJSON).then(value => {
+                finalResponse = [...value, ...finalResponse]
+                delay(10000);
+                processStocks(fmcgInputJSON).then(value => {
+                    finalResponse = [...value, ...finalResponse]
+                    delay(10000);
+                    processStocks(bankInputJSON).then(value => {
+                        finalResponse = [...value, ...finalResponse]
+                        var xls = json2xls(finalResponse);
+                        console.log("aFTER pROCess", finalResponse);
+                        fs.writeFileSync('stocks.xlsx', xls, 'binary');
+                    });
+                });
+            });
+
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
